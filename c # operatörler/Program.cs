1698a8542adf9d16b6b6791733f8c45b6b679cb3
İbrahim Rauf Alper <119485615+ibrahimrauf@users.noreply.github.com>
2023-01-06 02:57:23 +0300

    Add files via upload

---
+++ c # operatörler/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 8, b = 7;
+Console.WriteLine("a sayısı: "+ a + ", b sayısı:"+b);
+
+Console.WriteLine("a sayısı: {0}, b sayısı: {1} ", a, b);
+
+Console.WriteLine("++a => {0}", (++a));
+Console.WriteLine("a+=2 => {0}", (a+=2));
+Console.WriteLine("a==b => {0}", (a==b));
+Console.WriteLine("a>b => {0}", (a>b));
+
+    
